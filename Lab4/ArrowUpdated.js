f4a465c8330272d0b26e04f5e4b14225a28c1150
Liam Ronan <liamronan16@gmail.com>
2023-05-03 12:59:29 +0100

    added lab 4

---
+++ Lab4/ArrowUpdated.js
@@ -0,0 +1,28 @@
+//Arrow class
+class ArrowUpdated {
+	constructor(x, y, rotation, c, speed, angle) {
+		//x and y properties of arrow
+		this.x = x;
+		this.y = y;
+		//rotation of arrow
+		this.rotation = rotation;
+		this.c = c;
+		//vx = relevant equation using speed and angle
+		//vy = relevant equation using speed and angle
+		
+	}
+	//function to draw the arrow using vertex instead of line
+	drawArrow {
+		
+	}
+
+	//rotate arrow in direction of movement
+	rotateToMovement() {
+		
+	}
+	//move arrow - same as for Ball class
+	moveArrow(){
+		
+	}
+
+}
