000101e70c1d55cbe17157f2686c45a65b2d953c
Liam Ronan <liamronan16@gmail.com>
2023-05-08 15:20:49 +0100

    added moving arrows

--- Lab4/ArrowUpdated.js
+++ Lab4/ArrowUpdated.js
@@ -1,28 +1,48 @@
 //Arrow class
 class ArrowUpdated {
-	constructor(x, y, rotation, c, speed, angle) {
+	constructor( _colour) {
+		this.velocity = createVector(random(-2, 2), random(-2, 2));
 		//x and y properties of arrow
-		this.x = x;
-		this.y = y;
+		this.pos = createVector(random(50, width - 50), random(50, height - 50))
 		//rotation of arrow
-		this.rotation = rotation;
-		this.c = c;
-		//vx = relevant equation using speed and angle
-		//vy = relevant equation using speed and angle
-		
+		// this.rotation = _rotation;
+		this.colour = _colour;
 	}
 	//function to draw the arrow using vertex instead of line
-	drawArrow {
-		
+	drawArrow() {
+		noStroke()
+		fill(this.colour)
+		push()
+			translate(this.pos.x, this.pos.y)
+			beginShape();
+			rotate(this.velocity.heading() + 90);
+				vertex(-50, 10);
+				vertex(0, 10);
+				vertex(0, -20);
+				vertex(30, 0);
+				vertex(0, 20);
+				vertex(0, -10);
+				vertex(-50, -10);
+			endShape(CLOSE);
+		pop()
 	}
 
-	//rotate arrow in direction of movement
-	rotateToMovement() {
-		
-	}
 	//move arrow - same as for Ball class
 	moveArrow(){
-		
+		this.pos.add(this.velocity);
+
+		if(this.pos.x > width) {
+            this.pos.x = 0;
+        }
+        if(this.pos.x < 0) {
+            this.pos.x = width;
+        }
+        if(this.pos.y > height) {
+            this.pos.y = 0;
+        }
+        if(this.pos.y < 0) {
+            this.pos.y = height;
+        }
 	}
 
 }
--- Lab4/app.js
+++ Lab4/app.js
@@ -1,8 +1,19 @@
+let arrows = [];
+let colours = [];
 
 function setup() {
-
+    createCanvas(500,500);
+    angleMode(DEGREES);
+    for (let i = 0; i < 5; i++) {
+        colours.push(color(random(0, 255), random(0, 255), random(0,255)));
+        arrows.push(new ArrowUpdated(colours[i]))
+    }
 }
 
 function draw() {
-    
+    background(0);
+    arrows.forEach(arrow => {
+        arrow.drawArrow();
+        arrow.moveArrow();
+    });
 }
